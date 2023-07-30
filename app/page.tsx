07e29350b5ff39685d7ef7b4f406fcf807673b48
iamvageesh <vageeshgn2005@gmail.com>
2023-07-30 21:02:08 +0530

    :memo: Added myself section with head section improvement

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,11 @@
-import Image from "next/image";
-import { BsDot } from "react-icons/bs";
+import Main from "@/components/Main";
+import Myself from "@/components/Myself";
 
 export default function Home() {
   return (
-    <div className="mt-20">
-      <div className="flex items-center justify-center">
-        <div className="">
-          <div className="font-bold drop-shadow-custom text-6xl">
-            <h1>Hi,</h1>
-            <p>
-              I&apos;m{" "}
-              <span className="bg-slate-900 text-background rounded-xl p-2">
-                Vageesh
-              </span>
-            </p>
-          </div>
-          <div className="flex gap-1 mt-3 text-xl text-slate-600 items-center">
-            <p>Web</p>
-            <BsDot />
-            <p>Mobile</p>
-            <BsDot />
-            <p>Software</p>
-          </div>
-        </div>
-        <div>
-          <Image
-            src="/coding3.svg"
-            className="w-2/3 mx-auto"
-            width={250}
-            height={250}
-            alt="img"
-          />
-        </div>
-      </div>
+    <div className="mt-20 flex flex-col">
+      <Main />
+      <Myself />
     </div>
   );
 }
---
+++ components/Myself.tsx
@@ -0,0 +1,29 @@
+const Myself = () => {
+  return (
+    <div className="mt-20 w-full">
+      <div className="w-3/5 flex flex-col justify-center mx-auto">
+        <h1 className="text-4xl font-semibold underline">#Myself</h1>
+        <p className="text-xl">
+          Hi, I&apos;m Vageesh from India, a programming enthusiast who loves
+          Linux and tech talks. I enjoy web development, mobile app development,
+          and software development, always using new tools and frameworks. As an
+          open-source developer, I believe in building on foundations and
+          contributing back to the community. Let&apos;s create something
+          amazing together!
+        </p>
+        {/* <p>
+          Hey there! I&apos;m Vageesh, a passionate programming enthusiast from
+          India. I adore Linux and tech talks, and you can often find me delving
+          into web development, mobile app development, and software
+          development. Always on the lookout for fresh tools and frameworks, I
+          love adding a touch of innovation to my projects. Being an open-source
+          developer, I find joy in giving back to the community and building
+          upon strong foundations. Join me on this coding adventure, and
+          together, let&apos;s create something remarkable! 🚀
+        </p> */}
+      </div>
+    </div>
+  );
+};
+
+export default Myself;
