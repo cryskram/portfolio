274cd218a29940ecdebeb6ad4fd26199bc623a0d
GNVageesh <vageeshgn2005@gmail.com>
2023-10-05 21:13:07 +0530

    social media links in the head page

--- components/social.tsx
+++ components/social.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import { FaDiscord, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Social = () => {
   return (
     <div className="">
       <div className="flex-col gap-4 text-3xl hidden md:flex">
-        <FaInstagram />
-        <FaGithub />
-        <FaTwitter />
-        <FaDiscord />
+        <Link href="https://www.instagram.com/gn_vageesh/">
+          <FaInstagram />
+        </Link>
+        <Link href="https://github.com/cryskram">
+          <FaGithub />
+        </Link>
+        <Link href="https://twitter.com/gn_vageesh">
+          <FaTwitter />
+        </Link>
+        <Link href="https://discord.com/users/924186564215582771">
+          <FaDiscord />
+        </Link>
       </div>
     </div>
   );
